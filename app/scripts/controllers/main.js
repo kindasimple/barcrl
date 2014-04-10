99adca65c255262691e207c56706e4e03e4d47c5
Evan White <kindasimple@gmail.com>
2014-04-10 15:01:51 -0400

    Add modal window to preferences UI. Include images in tour results

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,5 +1,50 @@
 'use strict';
 
+//.controller('ModalInstanceCtrl', ['$scope', '$modalInstance', '$items', function ($scope, $modalInstance, items) {
+var ModalInstanceCtrl = function ($scope, $modalInstance, preferences) {
+  $scope.preferences = preferences;
+
+  //initiate cost slider parms
+  $scope.selectedCost = $scope.preferences.cost;
+  $scope.optionsCost = {
+    from: 1,
+    to: 100,
+    step: 1,
+    dimension: '  $$'
+  };
+    
+  //initiate alc slider parms
+  $scope.selectedAlcohol = $scope.preferences.alcohol;
+  $scope.optionsAlc = {
+    from: 1,
+    to: 100,
+    step: 1,
+    dimension: ''
+  };
+    
+  //initiate distance slider parms
+  $scope.selectedDistance = $scope.preferences.distance;
+  $scope.optionsDist = {
+    from: 1,
+    to: 100,
+    step: 1,
+    dimension: ''
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close({
+      cost: $scope.selectedCost,
+      alcohol: $scope.selectedAlcohol,
+      distance: $scope.selectedDistance
+    });
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
+//}]);
+
 angular.module('module.controller', ['module.service'])
   .controller('MainCtrl', ['$scope', function ($scope) {
     $scope.bars = [
@@ -9,34 +54,7 @@ angular.module('module.controller', ['module.service'])
     ];
   }])
 
-  .controller('CrawlCtrl', ['$scope', 'crawlrService', 'cfpLoadingBar', function($scope, crawlrService, cfpLoadingBar){
-	  
-	//initiate cost slider parms
-    $scope.costValue = '50';
-    $scope.optionsCost = {
-      from: 1,
-      to: 100,
-      step: 1,
-      dimension: '  $$'
-    };
-      
-    //initiate alc slider parms
-    $scope.alcValue = '50';
-    $scope.optionsAlc = {
-      from: 1,
-      to: 100,
-      step: 1,
-      dimension: ''
-    };
-      
-    //initiate distance slider parms
-    $scope.distValue = '50';
-    $scope.optionsDist = {
-      from: 1,
-      to: 100,
-      step: 1,
-      dimension: ''
-    };
+  .controller('CrawlCtrl', ['$scope', '$modal', '$log', 'crawlrService', 'cfpLoadingBar', function($scope, $modal, $log, crawlrService, cfpLoadingBar){
 	  
     $scope.start = 'Inferno';
 
@@ -76,9 +94,6 @@ angular.module('module.controller', ['module.service'])
       });
     
     $scope.refineTour=function(){
-     
-      
-
       crawlrService.getPreferenceRouteRequestId($scope.costValue,$scope.alcValue,$scope.disValue,$scope.start)
         .then(function(result) {
           showStatusAsBusy('Creating a Custom Bar Crawl based on your preferences.');
@@ -93,5 +108,33 @@ angular.module('module.controller', ['module.service'])
           }, 7000);
         });
     };
+
+    $scope.preferences = {
+      cost: '50',
+      alcohol: '50',
+      distance: '50'
+    };
+
+    $scope.open = function () {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'crawlPreferences.html',
+        controller: ModalInstanceCtrl,
+        resolve: {
+          preferences: function () {
+            return $scope.preferences;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (preferences) {
+        $scope.preferences = preferences;
+      }, function () {
+        $log.info('Modal dismissed at: ' + new Date());
+      });
+    };
+
   }]);
+
+
 
--- app/scripts/filters/filters.js
+++ app/scripts/filters/filters.js
@@ -1,3 +1,4 @@
+'use strict';
 angular.module('module.filter', [])
 
   .filter('ReplaceDots', function(){
